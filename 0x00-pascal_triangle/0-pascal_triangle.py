0104f8a519c2b2b295016ff2721541fbdf0a0534
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-01 19:56:28 +0000

    complete task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 
 """This module simulates an interview question"""
 
+
 def pascal_triangle(n):
     """Function returns a list of lists of
     integers representing the Pascal's triangle of n
@@ -12,5 +13,25 @@ def pascal_triangle(n):
         list: returns a list of lists
     """
     triangle = []
+    lastIndex = -1
     if n <= 0:
         return triangle
+
+    for i in range(1, n + 1):
+        if lastIndex == -1:
+            row = [1]
+            lastIndex = 0
+            triangle.append(row)
+        else:
+            row = [1]
+            last = triangle[lastIndex]
+
+            for i in range(len(last)):
+                if i + 1 < len(last):
+                    sum = last[i] + last[i + 1]
+                    row.append(sum)
+                else:
+                    row.append(last[i])
+            triangle.append(row)
+            lastIndex = lastIndex + 1
+    return triangle
