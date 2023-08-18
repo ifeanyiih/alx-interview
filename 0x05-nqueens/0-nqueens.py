379f1c9dc796ea4b689905d4650fe136e4fc6f37
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-18 04:53:53 +0000

    add limits to the program

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -30,4 +30,16 @@ def NQueens(n):
 
 if __name__ == "__main__":
     N = sys.argv[1]
+    if len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        N = int(N)
+    except:
+        print("N must be a number")
+        exit(1)
+    else:
+        if N < 4:
+            print("N must be at least 4")
+            exit(1)
     NQueens(int(N))
