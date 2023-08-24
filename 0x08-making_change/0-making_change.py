4b77491f5d2192c73b49a170868833e84b293674
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-24 20:15:51 +0000

    Modify: fix pep8 code style

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+
 def makeChange(coins, total):
     count = 0
     rem = total
     sortDesc = sorted(coins, reverse=True)
     if total <= 0:
         return 0
-
     while rem > 0:
         for coin in sortDesc:
             rel = rem // coin
@@ -14,5 +14,4 @@ def makeChange(coins, total):
             count += rel
             if rel == 0 and rem != 0:
                 return -1
-
     return count
