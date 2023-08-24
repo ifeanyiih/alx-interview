24489a610935049251a2ee6612d86ad0c3fccbe2
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-24 20:35:09 +0000

    Modify: Fix program logic

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,11 +10,12 @@ def makeChange(coins: list, total: int) -> int:
     sortDesc = sorted(coins, reverse=True)
     if total <= 0:
         return 0
-    while rem > 0:
-        for coin in sortDesc:
-            rel = rem // coin
-            rem = rem % coin
-            count += rel
-            if rel == 0 and rem != 0:
-                return -1
+    for coin in sortDesc:
+        rel = rem // coin
+        if rel <= 0:
+            continue
+        rem = rem % coin
+        count += rel
+    if rem > 0:
+        return -1
     return count
