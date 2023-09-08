f3942cb5653e42dc072ed575c6cf56531ece8eee
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-09-08 10:35:34 +0000

    Modify: make isPrime function more efficient

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,13 +11,12 @@ def isPrime(n):
     Returns:
         Bool: True or False
     """
-    isPrime = True
-    if n <= 1:
+    if n < 2:
         return False
-    for i in range(2, n):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            isPrime = False
-    return isPrime
+            return False
+    return True
 
 
 def isWinner(x, nums):
@@ -30,16 +29,14 @@ def isWinner(x, nums):
     Returns:
         winner name: str
     """
-    rounds = [list(range(1, i + 1)) for i in nums]
+    rounds = [iter((n for n in range(i + 1) if isPrime(n))) for i in nums]
     Maria = 0
     Ben = 0
     Winner = None
     for i in range(x):
-        round = rounds[i]
         count = 1
-        round_iterator = iter((p for p in round if isPrime(p)))
         while True:
-            prime = next(round_iterator, None)
+            prime = next(rounds[i], None)
             if prime is None:
                 if count % 2 == 0:
                     Maria += 1
